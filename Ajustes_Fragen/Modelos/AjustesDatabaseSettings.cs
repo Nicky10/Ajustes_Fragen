7e557d886e1e181d485ad0bdf2ed5065883dd49f
NicolasDelgado <nicolas.felipedelgado@gmail.com>
2020-09-28 10:08:25 -0500

    Subida de archivos

---
+++ Ajustes_Fragen/Modelos/AjustesDatabaseSettings.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Ajustes_Fragen.Modelos
+{
+    public class AjustesDatabaseSettings : IAjustesDatabaseSettings
+    { 
+        public string AjustesCollectionName { get; set; }
+        public string ConnectionString { get; set; }
+        public string DatabaseName { get; set; }
+    }
+
+    public interface IAjustesDatabaseSettings
+    {
+        string AjustesCollectionName { get; set; }
+        string ConnectionString { get; set; }
+        string DatabaseName { get; set; }
+    }
+}
